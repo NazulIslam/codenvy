1b2af3e4b71cdc8471ed67769deb13a640de4cdc
Saket Verma <saket_savi@icloud.com>
2015-07-16 21:42:41 +0000

    init

---
+++ src/main/java/com/codenvy/example/java/App.java
@@ -0,0 +1,11 @@
+package com.codenvy.example.java;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My Name is Saket");
+    }
+}
