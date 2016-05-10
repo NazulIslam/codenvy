58289e8f8ebd00c0aabf849631ed2e966bc7f175
Anonymous <nazul.islam@gmail.com>
2016-05-10 02:07:25 +0000

    init

--- src/main/java/com/codenvy/example/java/App.java
+++ src/main/java/com/codenvy/example/java/App.java
@@ -6,6 +6,6 @@ package com.codenvy.example.java;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("My Name is Saket");
+        System.out.println("My Name is Nazul");
     }
 }
